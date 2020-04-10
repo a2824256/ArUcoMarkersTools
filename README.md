76428c1a0e38adca5e993bdff9f2f97ba8caa8ea
a2824256 <lygg725@163.com>
2020-04-10 10:09:53 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ArUcoMarkersTools
 用于计算摄像头内参与测试ArUcoMarkers
+
+### 项目结构
+showChessboard.py ----- 用于生成棋盘格图片
+main.py           ----- 计算使用摄像头拍下棋盘格的图片的摄像头的内参(棋盘标定法)
+
+### 棋盘法相机内参标定流程
+##### 1.使用showChessboard.py生成棋盘图片
+##### 2.使用摄像头拍下棋盘的图片
+##### 3.使用main.py计算相机内参
+##### 4.下图为标定效果
+![test_result](./img/TestResult.jpg)
--- main.py
+++ main.py
@@ -7,12 +7,13 @@ import matplotlib.patches as patches
 import time
 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-# index由0开始
+# 10x8黑白间隔棋盘，由于是计算棋盘内顶点，所以长宽减1
 w = 9
 h = 7
 objp = np.zeros((w*h,3), np.float32)
 objp[:,:2] = np.mgrid[0:w,0:h].T.reshape(-1,2)
-objp = objp*18
+# 18mm为实际棋盘边长
+objp = objp * 18
 objpoints = []
 imgpoints = []
 images = glob.glob('chessboard.jpg')
@@ -20,6 +21,7 @@ i = 1
 for fname in images:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # 找到棋盘内顶点
     ret, corners = cv2.findChessboardCorners(gray, (w, h), None)
 
     if ret == True:
@@ -28,12 +30,14 @@ for fname in images:
         cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
     objpoints.append(objp)
     imgpoints.append(corners)
+    # 画棋盘角点
     cv2.drawChessboardCorners(img, (w, h), corners, ret)
     cv2.namedWindow('findCorners', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('findCorners', 1440, 1080)
+    cv2.resizeWindow('findCorners', 900, 700)
     cv2.imshow('findCorners', img)
 
     # cv2.destroyAllWindows()
+    # 计算摄像头内参
     ret, mtx, dist, rvecs, tvecs = \
     cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
     # 返回
